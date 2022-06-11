0b22c7fe8eeb6a8d1b79bf3b18c94b5ec312a15b
Jon Harmon <jonthegeek@gmail.com>
2022-06-11 12:52:34 -0500

    Use shinyslack_app to simplify run_app. (#67)

--- R/run_app.R
+++ R/run_app.R
@@ -2,29 +2,17 @@
 #'
 #' This function launches a Shiny App to help mentors find unanswered questions.
 #'
-#' @param local A logical indicating whether you are launching the app locally,
-#'   rather than on shinyapps.io. Useful for debugging toward a deployed version
-#'   of this app.
+#' @param ... arguments to pass to golem_opts. See
+#'   \code{\link[golem]{get_golem_options}} for more details.
 #'
 #' @export
 run_app <- function(local = interactive(), ...) {
-  if (local) {
-    site_url <- "http://127.0.0.1:4242/"
-  } else {
-    site_url <- production_site_url
-  }
-  app <- shiny::shinyApp(
-    ui = shinyslack::slack_shiny_ui(
-      ui = .app_ui,
-      team_id = team_id,
-      site_url = site_url
-    ),
-    server = .app_server
+  app <- shinyslack::shinyslack_app(
+    ui = .app_ui,
+    server = .app_server,
+    team_id = .team_id,
+    site_url = .site_url
   )
-  if (local) {
-    app$options$port <- 4242L
-    app$options$launch.browser <- TRUE
-  }
 
   golem::with_golem_options(
     app = app,
