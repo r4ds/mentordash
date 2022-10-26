2ffb5442c3211e14e164f809d5a4b271c9b61346
Jon Harmon <jonthegeek@gmail.com>
2022-10-26 16:33:29 -0500

    Small visible change.
    Let's make sure the existing forgotten GHA actually works.

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,7 +3,7 @@
 #' @return A \code{\link[shiny]{tagList}} containing the UI.
 #' @keywords internal
 .app_ui <- function() {
-  dashboard_title <- "R4DS Mentor Tool"
+  dashboard_title <- "R4DS Mentor Tool 2022-10-26"
   shinydashboard::dashboardPage(
     header = .ui_header(dashboard_title),
     sidebar = .ui_sidebar(),
